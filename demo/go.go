6c16c4c843154d8bd8d89d79f7f873b46f76866a
Chase Adams <realchaseadams@gmail.com>
2019-05-16 21:24:28 -0700

    update demo files

--- demo/go.go
+++ demo/go.go
@@ -2,8 +2,16 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 )
 
 func main() {
-	fmt.Println("Hello world!")
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Welcome to my website!")
+	})
+
+	fs := http.FileServer(http.Dir("static/"))
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
+
+	http.ListenAndServe(":80", nil)
 }
