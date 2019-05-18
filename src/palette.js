084f501ded8bd306b9d956eac7b65571d5152f44
Chase Adams <realchaseadams@gmail.com>
2019-05-17 23:23:03 -0700

    add color palette

---
+++ src/palette.js
@@ -0,0 +1,25 @@
+const colors = {
+  blackest: "#131417",
+  black: "#20242D",
+  blacklight: "#343740",
+  blackout: "#1E2129",
+  white: "#C7C8FF",
+  white77: "#C7C8FF77",
+  white33: "#C7C8FF33",
+  cyan: "#8CE8ff",
+  blue: "#69B4F9",
+  blue77: "#69B4F977",
+  blue33: "#69B4F933",
+  magenta: "#DD92F6",
+  magenta22: "#DD92F622",
+  magenta33: "#DD92F633",
+  magenta77: "#DD92F677",
+  magentaMuted: "#DD92F633",
+  red: "#FF8E8E",
+  green: "#7EFDD0",
+  green77: "#7EFDD077",
+  green33: "#7EFDD033",
+  yellow: "#FCAD3F"
+};
+
+module.exports = colors;
