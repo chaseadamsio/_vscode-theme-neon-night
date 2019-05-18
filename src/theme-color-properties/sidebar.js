03b8e1dba762e2c61c43dbd5156b8fbbf1b0d26e
Chase Adams <realchaseadams@gmail.com>
2019-05-18 14:36:20 -0700

    README: add color pallete

---
+++ src/theme-color-properties/sidebar.js
@@ -0,0 +1,11 @@
+const SIDEBAR = {
+  SIDEBARTITLE_FOREGROUND: `sideBarTitle.foreground`,
+  SIDEBARSECTIONHEADER_BORDER: `sideBarSectionHeader.border`,
+  SIDEBARSECTIONHEADER_BACKGROUND: `sideBarSectionHeader.background`,
+  SIDEBARSECTIONHEADER_FOREGROUND: `sideBarSectionHeader.foreground`,
+  SIDEBAR_BORDER: `sideBar.border`,
+  SIDEBAR_BACKGROUND: `sideBar.background`,
+  SIDEBARTITLE_FOREGROUND: `sideBarTitle.foreground`
+};
+
+module.exports = SIDEBAR;
