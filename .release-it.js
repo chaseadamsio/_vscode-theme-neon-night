fbbf1192f86a6ae34268662444c9d04c92a6443d
Chase Adams <realchaseadams@gmail.com>
2019-05-18 18:03:15 -0700

    add src/changelog.hbs for auto-changelog

--- .release-it.js
+++ .release-it.js
@@ -2,10 +2,13 @@ module.exports = {
   npm: false,
   github: {
     release: true,
-    releaseNotes: true,
-    changelog: "npx auto-changelog -p --stdout --commit-limit false"
+    releaseNotes: true
+  },
+  git: {
+    changelog:
+      "npx auto-changelog --stdout --commit-limit false -u --template ./src/changelog.hbs"
   },
   scripts: {
-    beforeStage: "npx auto-changelog -p"
+    beforeStage: "npx auto-changelog -p --template ./changelog.hbs"
   }
 };
