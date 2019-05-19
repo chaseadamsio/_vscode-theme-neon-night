5c5b7c1a8f74eb2b751ff6d481ec5e594d664146
Chase Adams <realchaseadams@gmail.com>
2019-05-18 18:57:40 -0700

    fix setting upstream

--- .github/actions/release/entrypoint.sh
+++ .github/actions/release/entrypoint.sh
@@ -4,5 +4,7 @@ echo "$SSH_PRIVATE_KEY" | ssh-add -
 git config --global user.email "caiovscerobot@gmail.com"
 git config --global user.name "CAIO VSCE Robot"
 
+git branch master -u origin/master
+
 yarn install
 yarn run release
