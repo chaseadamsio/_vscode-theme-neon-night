f6693549784c614f2cddb45fbc24b3b70ab36d63
Chase Adams <realchaseadams@gmail.com>
2019-05-18 17:24:19 -0700

    fix alpine ssh-add cmd

--- .github/actions/release/entrypoint.sh
+++ .github/actions/release/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 eval $(ssh-agent -s)
-ssh-add <(echo "$GIT_SSH_PRIV_KEY")
+echo "$SSH_PRIVATE_KEY" | ssh-add -
 git config --global user.email "caiovscerobot@gmail.com"
 git config --global user.name "CAIO VSCE Robot"
 
