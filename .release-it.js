a1fb3b7fc27f4d4d619e7c3679c1dec240bc6bc0
Chase Adams <realchaseadams@gmail.com>
2019-05-18 22:07:37 -0700

    re-enable release and release notes

--- .release-it.js
+++ .release-it.js
@@ -1,8 +1,7 @@
 module.exports = {
-  //   npm: false (commenting out but leaving, )
   github: {
-    release: false,
-    releaseNotes: false
+    release: true,
+    releaseNotes: true
   },
   git: {
     changelog:
