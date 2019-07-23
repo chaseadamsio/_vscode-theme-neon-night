67f31c938cf9425d8af6da55f4ed1ebf9d808fe8
Chase Adams <realchaseadams@gmail.com>
2019-07-22 21:30:11 -0700

    quickOpen: add colors for quick open

---
+++ src/colors/quick-input.js
@@ -0,0 +1,16 @@
+// @flow
+
+/* ::
+import type {Palette} from '../types'
+*/
+
+
+// https://code.visualstudio.com/api/references/theme-color#quick-picker
+const quickInput = (palette /*: Palette */) => ({
+    "quickInput.background": palette.blackest,
+    "quickInput.foreground": palette.white
+});
+
+module.exports = quickInput;
+
+
