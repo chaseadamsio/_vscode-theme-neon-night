b6de1e853db556bbdae93ea6656469f5231a8afb
Chase Adams <realchaseadams@gmail.com>
2019-07-22 21:32:26 -0700

    fix lint issue in quick-input

--- src/colors/quick-input.js
+++ src/colors/quick-input.js
@@ -4,13 +4,10 @@
 import type {Palette} from '../types'
 */
 
-
 // https://code.visualstudio.com/api/references/theme-color#quick-picker
 const quickInput = (palette /*: Palette */) => ({
-    "quickInput.background": palette.blackest,
-    "quickInput.foreground": palette.white
+  "quickInput.background": palette.blackest,
+  "quickInput.foreground": palette.white
 });
 
 module.exports = quickInput;
-
-
