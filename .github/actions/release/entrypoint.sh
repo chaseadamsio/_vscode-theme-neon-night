6bf7570448a3f4bafc767c4ac9ba8e8b04ca6af0
Chase Adams <realchaseadams@gmail.com>
2019-12-01 06:50:50 -0800

    maybe releasing is fine now?

--- .github/actions/release/entrypoint.sh
+++ .github/actions/release/entrypoint.sh
@@ -1,9 +1,3 @@
 #!/bin/sh
-eval $(ssh-agent -s)
-git config --global user.email "caiovscerobot@gmail.com"
-git config --global user.name "CAIO VSCE Robot"
-
-git branch master -u origin/master
-
-yarn install
-yarn run release
+yarn install \
+  && yarn run release
